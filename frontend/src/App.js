ef3aa2247e18fdbd1c93e8ca9264dee699b7c285
Radu <raduiris2007@gmail.com>
2023-06-05 16:23:42 +0300

    only now I did the initial design, switch enabled between pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
-    <div className="App"> </div>
+    <div className="App">
+      <MainPage/>  
+    </div>
   );
 }
 
