49f76425d32597bdce418afe6a68f889f8bb34c5
Radu <raduiris2007@gmail.com>
2023-06-05 18:15:25 +0300

    routing added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import MainPage from './components/MainPage';
+import {BrowserRouter as Router,Routes, Route, Link } from "react-router-dom"
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <MainPage/>  
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/">Main Page</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route exact path='/' element={< MainPage />}></Route>
+          <Route exact path='/about' element={< About />}></Route>
+        </Routes>
+
+      </div>
+    </Router>
+);
 }
 
 export default App;
