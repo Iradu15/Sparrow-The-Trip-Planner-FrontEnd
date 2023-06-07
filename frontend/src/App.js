cd187688ae54955b22ffff505539f3db00bb5dad
Toma <92331145+playback0022@users.noreply.github.com>
2023-06-07 20:46:28 +0300

    implemented onClick

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from "react";
 import MainMap from "./map-components/MainMap";
 import { Container } from "@mui/material";
 import { Marker } from "@react-google-maps/api";
@@ -6,12 +7,14 @@ import { Marker } from "@react-google-maps/api";
 const position = [{lat: 44.442787, lng: 26.064087}, {lat: 44.429809, lng: 26.100619}];
 
 function App() {
-  return <Container maxWidth="xxl">
-      <MainMap>
-          {/*<Marker position={position[0]}/>*/}
-          {/*<Marker position={position[1]}/>*/}
+    const [markerCoordinates, setMarkerCoordinates] = useState(null);
+
+    return <Container maxWidth="xxl">
+      <MainMap setMarkerCoordinatesInParent={setMarkerCoordinates}>
+          <Marker position={position[0]}/>
+          <Marker position={position[1]}/>
       </MainMap>
-  </Container>;
+    </Container>;
 }
 
 export default App;
