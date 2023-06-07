ae1fbb74d92c1080aeac16df8c7577a124d5204f
Radu <raduiris2007@gmail.com>
2023-06-07 13:03:50 +0300

    signup is working now. At bdate there is a problem with the format because it expects dd.mm.yyyy but the format of the form is with / instead of .

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const MainPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [email, setEmail] = useState("");
+  const [profilePhoto, setProfilePhoto] = useState("");
+  const [first_name, setFirst_name] = useState("");
+  const [last_name, setLast_name] = useState("");
   const [error, setError] = useState("");
   const [isSignup, setIsSignup] = useState(false);
 
@@ -11,40 +17,28 @@ const MainPage = () => {
     e.preventDefault();
 
     try {
+
+      // Perform signup logic
       if (isSignup) {
-        // Perform signup logic
-      
-        //login to django admin interface with parola+username pizdoase
-        const response = await axios.post(
-          "http://127.0.0.1:8000/admin/login/?next=/admin/",
-          {
-            username: "admin",
-            password: "admin",
-          }
-        );
         
-        // Check the response status to determine if the login was successful
-        if (response.status === 200) {
-          // Login successful, redirect to the desired admin page
-          window.location.href =
-            "http://127.0.0.1:8000/admin/auth/user/add/";
-
-            // const response2 = await axios.post(
-            //   "http://127.0.0.1:8000/admin/login/",
-            //   {
-            //     username,
-            //     password,
-            //   }
-            // );
-
-        } else {
-          // Login failed, handle the error
-          setError("Login failed. Please try again.");
-        }
-
+        const response = await axios.post("http://127.0.0.1:8000/member/list/", {
+          "baseUser": {
+            "username": username,
+            "password": password,
+            "passwordCheck": passwordCheck,
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email
+          },
+          "birthDate": "09.11.1990"
+        });
         console.log("Signup successful:", response.data);
-      } else {
-        // Perform login logic
+
+      } 
+      
+       // Perform login logic
+      else {
+       
         const response = await axios.post("http://127.0.0.1:8000/auth/login/", {
           username,
           password,
@@ -55,10 +49,27 @@ const MainPage = () => {
       // Clear form inputs and error message
       setUsername("");
       setPassword("");
+      setEmail("")
+      setPasswordCheck("")
       setError("");
     } catch (error) {
-      setError("Error occurred. Please try again."); // Set the error message based on the response from the API
-    }
+        console.log(error)
+        // Handle the error response
+        if (error.response) {
+          // The request was made and the server responded with an error status
+          const errorData = error.response.data;
+          const errorMessage = errorData.detail || "Error occurred. Please try again.";
+          setError(errorMessage);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+          setError("No response received from the server.");
+        } else {
+          // Something happened in setting up the request that triggered an error
+          console.log("Error:", error.message);
+          setError("Error occurred. Please try again.");
+        }
+      }
   };
 
   return (
@@ -84,11 +95,37 @@ const MainPage = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
           {isSignup && (
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              // Add necessary logic for confirming password
-            />
+            <>
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                value={passwordCheck}
+                onChange={(e) => setPasswordCheck(e.target.value)}
+              />
+
+              <input 
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            
+              <input 
+                type="text"
+                placeholder="First name"
+                value={first_name}
+                onChange={(e) => setFirst_name(e.target.value)}
+              />
+
+
+              <input 
+                type="text"
+                placeholder="Last name"
+                value={last_name}
+                onChange={(e) => setLast_name(e.target.value)}
+              />
+            </>
+            
           )}
           <button type="submit">{isSignup ? "Sign Up" : "Login"}</button>
         </form>
