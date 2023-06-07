166eb18d68dffdd73d20ea79db10c86b9ae7149d
Radu <raduiris2007@gmail.com>
2023-06-07 14:51:19 +0300

    the same login boxes in both pages

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Cookies from "js-cookie";
+import { Paper, TextField, Button, Typography, Link, Snackbar } from "@mui/material";
 
 const MainPage = () => {
   const [username, setUsername] = useState("");
@@ -11,66 +11,75 @@ const MainPage = () => {
   const [first_name, setFirst_name] = useState("");
   const [last_name, setLast_name] = useState("");
   const [error, setError] = useState("");
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
   const [isSignup, setIsSignup] = useState(false);
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
 
     try {
-
       // Perform signup logic
       if (isSignup) {
-        
         const response = await axios.post("http://127.0.0.1:8000/member/list/", {
-          "baseUser": {
-            "username": username,
-            "password": password,
-            "passwordCheck": passwordCheck,
-            "first_name": first_name,
-            "last_name": last_name,
-            "email": email
+          baseUser: {
+            username,
+            password,
+            passwordCheck,
+            first_name,
+            last_name,
+            email,
           },
-          "birthDate": "09.11.1990"
+          birthDate: "09.11.1990",
         });
-        console.log("Signup successful:", response.data);
-
-      } 
-      
-       // Perform login logic
-      else {
-       
+        setSuccessMessage("SignUp successful!");
+      } else {
+        // Perform login logic
         const response = await axios.post("http://127.0.0.1:8000/auth/login/", {
           username,
           password,
         });
-        console.log("Login successful:", response.data);
+        setSuccessMessage("Login successful!");
       }
 
       // Clear form inputs and error message
       setUsername("");
       setPassword("");
-      setEmail("")
-      setPasswordCheck("")
-      setError("");
+      setEmail("");
+      setPasswordCheck("");
+      setError("");  
+      setShowSuccess(true);
+      
     } catch (error) {
-        console.log(error)
-        // Handle the error response
-        if (error.response) {
-          // The request was made and the server responded with an error status
-          const errorData = error.response.data;
-          const errorMessage = errorData.detail || "Error occurred. Please try again.";
-          setError(errorMessage);
-        } else if (error.request) {
-          // The request was made but no response was received
-          console.log(error.request);
-          setError("No response received from the server.");
-        } else {
-          // Something happened in setting up the request that triggered an error
-          console.log("Error:", error.message);
-          setError("Error occurred. Please try again.");
-        }
+      // Handle the error response
+      if (error.response) {
+        // The request was made and the server responded with an error status
+        const errorData = error.response.data;
+        const errorMessage =
+          errorData.detail || "Error occurred. Please try again.";
+        setError(errorMessage);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log(error.request);
+        setError("No response received from the server.");
+      } else {
+        // Something happened in setting up the request that triggered an error
+        console.log("Error:", error.message);
+        setError("Error occurred. Please try again.");
       }
+    }
   };
+
+  useEffect(() => {
+    let timer;
+    if (showSuccess) {
+      timer = setTimeout(() => {
+        setShowSuccess(false);
+      }, 3000); // Duration in milliseconds (3 seconds)
+    }
+    return () => clearTimeout(timer);
+  }, [showSuccess]);
+
 
   return (
     <div className="login-page">
@@ -78,67 +87,85 @@ const MainPage = () => {
         <h2>Welcome to the App!</h2>
         <p>This is a description of the app.</p>
       </div>
+
+        <div> <Snackbar
+            open={showSuccess}
+            autoHideDuration={3000}
+            onClose={() => setShowSuccess(false)}
+            message={successMessage}
+        /></div>
+
+
       <div className="form-container">
-        <h2>{isSignup ? "Sign Up" : "Login"}</h2>
-        {error && <p className="error">{error}</p>}
-        <form onSubmit={handleFormSubmit}>
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {isSignup && (
-            <>
-              <input
-                type="password"
-                placeholder="Confirm Password"
-                value={passwordCheck}
-                onChange={(e) => setPasswordCheck(e.target.value)}
-              />
+  
+        <Paper elevation={3} sx={{ padding: "2rem" }}>
+          <Typography variant="h2">{isSignup ? "Sign Up" : "Login"}</Typography>
+          {error && <Typography color="error">{error}</Typography>}
+          <form onSubmit={handleFormSubmit}>
+            <TextField
+              type="text"
+              label="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              sx={{ marginBottom: "1rem" }}
+            />
+            <TextField
+              type="password"
+              label="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              sx={{ marginBottom: "1rem" }}
+            />
+            {isSignup && (
+              <>
+                <TextField
+                  type="password"
+                  label="Confirm Password"
+                  value={passwordCheck}
+                  onChange={(e) => setPasswordCheck(e.target.value)}
+                  sx={{ marginBottom: "1rem" }}
+                />
+                <TextField
+                  type="email"
+                  label="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  sx={{ marginBottom: "1rem" }}
+                />
+                <TextField
+                  type="text"
+                  label="First name"
+                  value={first_name}
+                  onChange={(e) => setFirst_name(e.target.value)}
+                  sx={{ marginBottom: "1rem" }}
+                />
+                <TextField
+                  type="text"
+                  label="Last name"
+                  value={last_name}
+                  onChange={(e) => setLast_name(e.target.value)}
+                  sx={{ marginBottom: "1rem" }}
+                />
+              </>
+            )}
 
-              <input 
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
+            <Button variant="contained" type="submit">
+              {isSignup ? "Sign Up" : "Login"}
+            </Button>
+            </form>
             
-              <input 
-                type="text"
-                placeholder="First name"
-                value={first_name}
-                onChange={(e) => setFirst_name(e.target.value)}
-              />
-
-
-              <input 
-                type="text"
-                placeholder="Last name"
-                value={last_name}
-                onChange={(e) => setLast_name(e.target.value)}
-              />
-            </>
-            
-          )}
-          <button type="submit">{isSignup ? "Sign Up" : "Login"}</button>
-        </form>
-        <p>
-          {isSignup
-            ? "Already have an account? "
-            : "Don't have an account? "}
-          <span className="link" onClick={() => setIsSignup(!isSignup)}>
-            {isSignup ? "Login" : "Sign up"}
-          </span>
-        </p>
+          <Typography>
+            {isSignup ? "Already have an account? " : "Don't have an account? "}
+            <Link onClick={() => setIsSignup(!isSignup)}>
+              {isSignup ? "Login" : "Sign up"}
+            </Link>
+          </Typography>
+       
+        </Paper>
       </div>
     </div>
+
+    
   );
 };
 
