196598ba5418e95254f16792aa36be416286382b
Toma <92331145+playback0022@users.noreply.github.com>
2023-06-07 15:34:01 +0300

    uninstalled legacy npm module (react-google-maps); implemented centering around provided markers when loading the map; implemented geolocation for maps where input should be provided; added the default travel-friendly style;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import MainMap from "./map-components/MainMap"
+import MainMap from "./map-components/MainMap";
+import { Container } from "@mui/material";
+import { Marker } from "@react-google-maps/api";
+
+const position = [{lat: 44.442787, lng: 26.064087}, {lat: 44.429809, lng: 26.100619}];
 
 function App() {
-  return <div className="container">
-    <div className="map">
-      <MainMap/>
-    </div>
-  </div>;
+  return <Container maxWidth="xxl">
+      <MainMap>
+          {/*<Marker position={position[0]}/>*/}
+          {/*<Marker position={position[1]}/>*/}
+      </MainMap>
+  </Container>;
 }
 
 export default App;
--- frontend/src/map-components/MainMap.js
+++ frontend/src/map-components/MainMap.js
@@ -1,17 +1,48 @@
-import { useLoadScript } from "@react-google-maps/api"
-import { useState, useMemo, useCallback, useRef } from "react"
+import { useJsApiLoader } from "@react-google-maps/api";
+import { useState, useMemo, useCallback, useRef } from "react";
+import { Children } from "react";
 import { GoogleMap, MarkerClusterer } from "@react-google-maps/api";
 
 
-const center = {lat: 44.435569, lng: 26.099511};
+// when no markers are provided, the map will be centered so that the whole world is visible
+const defaultCenter = {lat: 45, lng: 0};
+// 'travel' map style; disable map type switch buttons
+const options = {mapId: "77ee2dda51aa3d0d", mapTypeControl: false};
 
-export default function MainMap({ children }) {
-    const {isLoaded} = useLoadScript({
-        googleMapsApiKey: "",
+// the onClick prop should be provided only in the context of a map from which input is expected (e.g. add attraction);
+// for purely output-generating maps (all others), when no markers are provided, it is appropriate for the map to go
+// into the whole-world view directly, without asking permission to acquire the user's current location;
+export default function MainMap({ children, onClick }) {
+    const {isLoaded} = useJsApiLoader({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     });
+
+    // hook which centers the map around the provided markers and sets the zoom appropriately;
+    // when no markers are provided, if the user chooses to provide their location, the map is
+    // centered accordingly;
+    // if all else fails, the map goes into the whole-world view
+    const onLoad = useCallback((map) => {
+        if (children !== undefined) {
+            const bounds = new window.google.maps.LatLngBounds();
+
+            // extend the bounds by each marker's coordinates
+            Children.forEach(children, (location) => {
+                bounds.extend(location.props.position);
+            })
+
+            map.fitBounds(bounds);
+        }
+        else if (onClick) {
+            navigator.geolocation.getCurrentPosition(
+                (currentPosition) => {
+                    map.panTo({lat: currentPosition.coords.latitude, lng: currentPosition.coords.longitude});
+                });
+        }
+    }, []);
 
     if (!isLoaded) return <>loading...</>;
 
-    return (<GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-        </GoogleMap>)
+    return <GoogleMap zoom={3.35} center={defaultCenter} mapContainerClassName="map-container" options={options} onLoad={onLoad} onClick={onClick}>
+                {children}
+        </GoogleMap>;
 }
