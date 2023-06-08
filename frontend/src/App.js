822d789e4bd6267ce94d9507e8a2b913266fb9c2
Toma <92331145+playback0022@users.noreply.github.com>
2023-06-08 15:48:15 +0300

    some work on the home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
 import { useState } from "react";
 import MainMap from "./map-components/MainMap";
+import Home from "./Home";
 import { Container } from "@mui/material";
 import { Marker } from "@react-google-maps/api";
 
 const position = [{lat: 44.442787, lng: 26.064087}, {lat: 44.429809, lng: 26.100619}];
 
 function App() {
-    const [markerCoordinates, setMarkerCoordinates] = useState(null);
-
-    return <Container maxWidth="xxl">
-      <MainMap setMarkerCoordinatesInParent={setMarkerCoordinates}>
-          <Marker position={position[0]}/>
-          <Marker position={position[1]}/>
-      </MainMap>
-    </Container>;
+    // const [markerCoordinates, setMarkerCoordinates] = useState(null);
+    // const [selectedAttractions, setSelectedAttractions] = useState([]);
+    // const [searchAttractions, setSearchAttractions] = useState([]);
+    //
+    // return <Container maxWidth="xxl">
+    //   <MainMap setMarkerCoordinatesInParent={setMarkerCoordinates}>
+    //       <Marker position={position[0]}/>
+    //       <Marker position={position[1]}/>
+    //   </MainMap>
+    // </Container>;
+    return <Home/>;
 }
 
 export default App;
