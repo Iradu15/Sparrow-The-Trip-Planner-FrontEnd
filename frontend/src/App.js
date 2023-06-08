a84f43f10c9e7e7c4412850227f5e25dcb8ac81e
VladWero08 <vlad_wero@yahoo.com>
2023-06-08 19:03:04 +0300

    Finished (?) design for side menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import { useState } from "react";
 import MainMap from "./map-components/MainMap";
 import Home from "./Home";
+import SideMenu from "./menu-components/SideMenu";
 import { Container } from "@mui/material";
 import { Marker } from "@react-google-maps/api";
 
 function App() {
-    return <Home/>;
+    return <SideMenu
+        title="Vatra Dornei"
+        publicationDate="118.20.23231"
+        author="John Wick"
+        description="John Wick a plecat in Vatra Dornei"
+    />;
 }
 
 export default App;
