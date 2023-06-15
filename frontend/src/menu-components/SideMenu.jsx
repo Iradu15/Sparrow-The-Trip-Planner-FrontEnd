c4959a319ad5d96e7ebbde1e320b81aefa3a23a2
IoanaTC <ioanateodoraconstantin@yahoo.com>
2023-06-15 04:34:01 +0300

    up and down

--- frontend/src/menu-components/SideMenu.jsx
+++ frontend/src/menu-components/SideMenu.jsx
@@ -1,54 +1,65 @@
 import "../assets/CSS/general-style.css";
 import "./SideMenu.css";
 import React, { useState , useEffect} from "react";
+import arrowDownImage from "../assets/images/arrows/arrow-down.png";
+import arrowUpImage from "../assets/images/arrows/arrow-up.png";
+
 
 export default function SideMenu({title, publicationDate, author, description, attractions}){
-    const [x, setX] = useState(window.innerWidth / 2);
     const [y, setY] = useState(window.innerHeight / 2);
-
+    const [prevY, setPrevY] = useState(window.innerHeight / 2 - 1);
     const [isExpanded, setIsExpanded] = useState(false);
+    const [direction, setDirection] = useState("down");
+    const [hamburgerImage, setHamburgerImage] = useState(arrowUpImage);
+    
 
     const handleToggleMenu = () => {
         setIsExpanded(!isExpanded);
     };
-    
+
     useEffect(() => {
         const handleMouseMove = (e) => {
-          if (!isExpanded) {
-            setY(e.pageY);
-          }
+            setPrevY(y);
+            setY(e.pageY);  
         };
-    
+
         window.addEventListener("mousemove", handleMouseMove);
-    
+
         return () => {
-          window.removeEventListener("mousemove", handleMouseMove);
+            window.removeEventListener("mousemove", handleMouseMove);
         };
-    }, [isExpanded]);
-    
+    }, [isExpanded, y]);
+
+    useEffect(() => {
+        if (prevY < y) {
+          setDirection("down");
+          setHamburgerImage(arrowDownImage);
+        }
+        if (prevY > y) {
+            
+          setDirection("up");
+          setHamburgerImage(arrowUpImage);
+        }
+      }, [prevY, y]);
 
     const hamburgerStyle = {
-        transform: `translateY(${y}px)`, // Update the translateY property to move the hamburger vertically
+        transform: `translateY(${y}px)`,
     };
+
 
     return (
         <div 
         className={`side__menu ${isExpanded ? "expanded" : ""}`}>
             
-            <div className="hamburger" style = {hamburgerStyle} onClick={handleToggleMenu}>
-                <div className="line"></div>
-                <div className="line"></div>
-                <div className="line"></div>
-		    </div>
-
+            <img src = {hamburgerImage}  className="hamburger"  style = {hamburgerStyle} onClick={handleToggleMenu} />
             <div className="btn__wrapper">
                 <div className="btn btn__edit">Edit</div>
                 <div className="btn btn__delete">Delete</div>
             </div>
 
             <div className="route__informations">
-                <h1 className="route__title">{title}</h1>
                 <h5 className="route__publication">{publicationDate} - {author}</h5>
+                <h1 className="route__title">{title}</h1>
                 <p className="route__description">{description}</p>
 
             </div>
